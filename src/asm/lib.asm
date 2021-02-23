8f985111ffffffef034a4be229539c3a9ca9482a
Alvin Wilta <51949757+alvinwilta@users.noreply.github.com>
2021-02-24 01:21:50 +0700

    fix readString()

--- src/asm/lib.asm
+++ src/asm/lib.asm
@@ -1,12 +1,13 @@
 ; lib.asm
 ; Alvin W., Josep M., Rehagana K.C.S.
-; 20 Februari 2020
+; 20 Februari 2021
 
 ; lib.asm berisi fungsi-fungsi bantuan kernel yang tidak bisa didefinisikan di
 ; file lib.c
 
 ; global _loadBin
 global _getCursorRow
+global _getCursorCol
 
 # int *loadBinary()
 ; _loadBin:
@@ -22,7 +23,7 @@ global _getCursorRow
 
 ; 	.binFile: incbin "out/image.bin"
 
-;int getCursor()
+;int getCursorCol()
 _getCursorRow:
 	push bp
 	mov bp,sp
@@ -34,3 +35,18 @@ _getCursorRow:
 	mov ah,0	;return row yg ditaro di AL
 	pop bp
 	ret
+
+;int getCursorCol()
+_getCursorCol:
+	push bp
+	mov bp,sp
+	mov ah,0x03
+	xor bh,bh
+	int 0x10
+
+	mov al,dl	;ambil col nya // al = dl
+	mov ah,0	;return col yg ditaro di AL
+	pop bp
+	ret
+
+;int getCursorCol()
--- src/c/kernel.c
+++ src/c/kernel.c
@@ -1,27 +1,33 @@
-// sudo ln -s "/mnt/c/path ke bochs installation>/bochs.exe" /bin/bochs
-// echo 'BXSHARE="/mnt/c/<foldernya bochs>"' >> ~/.bashrc
 /**
  * kernel.c
  * Alvin W., Josep M., Rehagana K.C.S.
- * 20 Februari 2020
+ * 20 Februari 2021
  */
 #include "kernel.h"
 #include "lib.h"
+#include "boolean.h"
 
 int main()
 {
+    char testo[DEFAULT_BUFFER_SIZE];
     // Set video mode
     // http://www.ctyme.com/intr/rb-0069.htm
     // 320x200 with 256 colors, 40x25 text resolution
-    /*interrupt(0x10, 0x0013, 0, 0, 0);*/
+    // interrupt(0x10, 0x0013, 0, 0, 0);
     // 640x200 with 16 colors, 80x30 text resolution
     interrupt(0x10, 0x0012, 0, 0, 0);
 
     // drawImage();
+    // bikinPersegi(128, LTCYAN);
     printLogo();
-    // printString("The quick brown fox jumps over the lazy dog\nlorem ipsum dor amet\nhello world");
-    /*bikinPersegi(128, LTCYAN);*/
-    while(1);
+    while(1)
+    {
+        printString("Tuliskan keluh-kesahmu hari ini: ");
+        readString(testo);
+        printString(testo);
+        printString("\n");
+        printString("The quick brown fox jumps over the lazy dog\nlorem ipsum dor amet\nhello world\n");
+    }
 }
 
 void handleInterrupt21(int AX, int BX, int CX, int DX)
@@ -61,36 +67,64 @@ void printString(char *string)
         i++;
     }
 }
-void readString(char *string) 
+void readString(char *string)
 {
     // perlu loop -> break pas pencet enter? ASCII enter: 0xd
     // perlu merhatiin backspace, ASCII bspc: 0x8
 
     int i = 0; // indeks
+    int kolom, baris,
+        minBaris = getCursorRow(), // biar ga nimpa ke atasnya pas backspace
+        minKolom = getCursorCol(),
+        maxKolom = minKolom; // buat liat kolom maksimumnya berapa
+    bool udah = false;
 
     while (1)
     {
+        kolom = getCursorCol();
+        baris = getCursorRow();
+
+        kolom = (kolom <= minKolom && baris <= minBaris) ? minKolom : kolom;
         string[i] = interrupt(0x16,0x0000,0x0000,0x0000,0x0000);
-        if (string[i] == 0x8 && i>0)
+        if ((string[i] == 0x8 && i>0) && !(baris == minBaris && kolom <= minKolom))
         {
             // backspace
             // idenya: mundurin cursor by 1, terus timpa karakter sblmnya dgn pixel hitam
             i--;
-            interrupt(0x10, 0x0C00 + BLACK, 0x0000, getCursorRow(), i);
+
+            if (kolom > 0)
+            {
+                kolom--;
+            }
+            else if (kolom == 0 && baris > minBaris)
+            {
+                baris--;
+                kolom = maxKolom;
+            }
+
+            interrupt(0x10, 0x0200, 0x0000, 0x0000, baris*0x100+kolom);
+            interrupt(0x10, 0x0920, 0x0000 + BLACK, 0x1, 0);
+
         }
         else if (string[i] == 0x0D)
         {
             printString("\n");
             break;
         }
-        else
+        else if (string[i] != 0x8)
         {
             interrupt(0x10, 0x0E00 + string[i], 0x0000 + WHITE, 0x0000, 0x0000);
             i++;
+            if ( !udah && baris == minBaris+1)
+            {
+                maxKolom += i-2; // KENAPA -2 ??????????????????????????????????
+                udah = true;
+            }
         }
     }
-
+    string[i++] = 0x0; // == '\0'
 }
+
 void bikinPersegi(int sisi, int warna)
 {
     int i, j, k = 0;
